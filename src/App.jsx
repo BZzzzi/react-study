5b8da127e46278ee2ea1b980b9312a4641483add
BZzzzi <khj23465@naver.com>
2025-10-20 21:05:58 +0900

    [React Hook] - 비동기 함수에서 리액트 State 업데이트 방식 예시

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const handleClick1 = () => {
+    setTimeout(() => {
+      setCount1(count1 + 1);
+    }, 2000);
+  };
+
+  const handleClick2 = () => {
+    setTimeout(() => {
+      setCount2((pre) => pre + 1);
+    }, 2000);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick1}>{count1}</button>
+      <button onClick={handleClick2}>{count2}</button>
+    </div>
+  );
 }
 
 export default App;
