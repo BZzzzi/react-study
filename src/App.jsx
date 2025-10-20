2729bfd09b7fd2a8facc547651bb5cb141f42863
BZzzzi <khj23465@naver.com>
2025-10-21 01:39:20 +0900

    [React Hook] - useEffect 활용 예시 - 사이드 이펙트 실행과 정리 함수 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import Toggle from "./Toggle";
+import { useEffect, useState } from "react";
+
+function Clock() {
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log(date);
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return <div>{date.toTimeString()}</div>;
+}
 
 function App() {
+  const [show, setShow] = useState(false);
+
   return (
-    <>
-      <Toggle />
-    </>
+    <div>
+      <button onClick={() => setShow(true)}>보이기</button>
+      <button onClick={() => setShow(false)}>숨기기</button>
+      {show && <Clock />}
+    </div>
   );
 }
 
