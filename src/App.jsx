8de1c17772137456fe59341da8fdfa1934a55075
BZzzzi <khj23465@naver.com>
2025-10-21 16:22:34 +0900

    [React Hook] - useRef + useEffect 활용 예시

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import Carousel from "./Carousel";
+import { useEffect } from "react";
+import { useRef } from "react";
 
 function App() {
-  const images = [
-    "https://picsum.photos/200/300?random=1",
-    "https://picsum.photos/200/300?random=2",
-    "https://picsum.photos/200/300?random=3",
-  ];
+  const focusRef = useRef(null);
+
+  const submit = (formData) => {
+    const username = formData.get("username");
+    const password = formData.get("password");
+    console.log({ username, password });
+  };
+
+  useEffect(() => {
+    if (focusRef.current) {
+      focusRef.current.focus();
+    }
+  }, []);
 
   return (
-    <div>
-      <Carousel images={images} />
-    </div>
+    <form action={submit}>
+      <input ref={focusRef} name="username" />
+      <input name="password" type="password" />
+      <button type="submit">확인</button>
+    </form>
   );
 }
 
