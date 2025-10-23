8c6c594625df43e3cf7b929dc66198688ae9c453
BZzzzi <khj23465@naver.com>
2025-10-23 23:27:58 +0900

    [React Hook] useContext() 활용 예시1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,68 @@
-import { useEffect } from "react";
-import { useRef } from "react";
+import { useContext, useState } from "react";
+import MyContext from "./MyContext";
 
-function App() {
-  const focusRef = useRef(null);
-
-  const submit = (formData) => {
-    const username = formData.get("username");
-    const password = formData.get("password");
-    console.log({ username, password });
-  };
-
-  useEffect(() => {
-    if (focusRef.current) {
-      focusRef.current.focus();
-    }
-  }, []);
+function E() {
+  const value = useContext(MyContext);
 
   return (
-    <form action={submit}>
-      <input ref={focusRef} name="username" />
-      <input name="password" type="password" />
-      <button type="submit">확인</button>
-    </form>
+    <div>
+      <p>컴포넌트 E</p>
+      <p>E에서 value: {value}</p>
+    </div>
+  );
+}
+
+function D() {
+  return (
+    <div>
+      <p>컴포넌트 D</p>
+      <E />
+    </div>
+  );
+}
+
+function C() {
+  const value = useContext(MyContext);
+
+  return (
+    <div>
+      <p>컴포넌트 C</p>
+      <p>C에서 value: {value}</p>
+      <D />
+    </div>
+  );
+}
+
+function B() {
+  return (
+    <div>
+      <p>컴포넌트 B</p>
+      <C />
+    </div>
+  );
+}
+
+function A() {
+  return (
+    <div>
+      <p>컴포넌트 A</p>
+      <B />
+    </div>
+  );
+}
+
+function App() {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <MyContext.Provider value={value}>
+        <p>App 컴포넌트</p>
+        <p>App의 value: {value}</p>
+        <A />
+        <button onClick={() => setValue(value + 1)}>value 증가</button>
+      </MyContext.Provider>
+    </div>
   );
 }
 
