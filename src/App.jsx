8eb47d3b6bffe26439aa081d4a9410f38e122448
BZzzzi <khj23465@naver.com>
2025-10-28 23:16:24 +0900

    [React useEffect] 파라미터를 활용한 예시1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [num, setNum] = useState(0);
 
-  const addCount = useCallback(() => {
+  const addCount = (log) => {
     setCount((c) => c + 1);
-    console.log(`num: ${num}`);
-  }, [num]);
+    console.log(log);
+  };
 
   const addNum = () => setNum((n) => n + 1);
 
   useEffect(() => {
     console.log("timer start");
     const timerId = setInterval(() => {
-      addCount();
+      addCount(`num: ${num}`);
     }, 1000);
 
     return () => {
       clearInterval(timerId);
       console.log("timer end");
     };
-  }, [addCount]);
+  }, [num]);
 
   return (
     <div>
