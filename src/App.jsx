1223c39c950b1d8ff7be808c30357fe5f8d79636
BZzzzi <khj23465@naver.com>
2025-10-29 15:56:19 +0900

    [Styled Components] 변수, 함수를 이용한 스타일 적용 - 삼항 연산자, 널 병합 연산자 등 이용

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,29 @@
 import styled from "styled-components";
 import Button from "./Button";
-import Input from "./Input";
 
 const Container = styled.div`
-  margin: 0 auto;
-  width: 400px;
-
-  ${Input} {
-    box-sizing: border-box;
-    display: block;
-    margin: 8px 0 16px;
-    width: 100%;
+  ${Button} {
+    margin: 10px;
   }
 `;
 
 function App() {
   return (
     <Container>
-      <h1>로그인</h1>
-      <label for="email">이메일</label>
-      <Input
-        type="email"
-        id="email"
-        class="input"
-        placeholder="styled@codeit.kr"
-      />
-      <label for="password">비밀번호</label>
-      <Input
-        type="password"
-        id="password"
-        class="input"
-        placeholder="비밀번호"
-      />
-      <Button>Hello Styled!</Button>
+      <h1>기본 버튼</h1>
+      <Button size="small">small</Button>
+      <Button size="medium">medium</Button>
+      <Button size="large">large</Button>
+      <h1>둥근 버튼</h1>
+      <Button size="small" round>
+        round small
+      </Button>
+      <Button size="medium" round>
+        round medium
+      </Button>
+      <Button size="large" round>
+        round large
+      </Button>
     </Container>
   );
 }
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,38 +1,27 @@
 import styled from "styled-components";
-import nailImg from "./assets/nail.png";
 
-const Icon = styled.img`
-  width: 16px;
-  height: 16px;
-`;
+const SIZES = {
+  large: 24,
+  medium: 20,
+  small: 16,
+};
 
 const StyledButton = styled.button`
   background-color: #6750a4;
   border: none;
+  border-radius: ${({ round }) => (round ? `9999px` : `3px`)};
   color: #ffffff;
+  font-size: ${({ size }) => SIZES[size] ?? SIZES["medium"]}px;
   padding: 16px;
-
-  ${Icon} {
-    margin-right: 4px;
-  }
 
   &:hover,
   &:active {
     background-color: #463770;
-
-    ${Icon} {
-      opacity: 0.2;
-    }
   }
 `;
 
 function Button({ children, ...props }) {
-  return (
-    <StyledButton {...props}>
-      <Icon src={nailImg} alt="nail icon" />
-      {children}
-    </StyledButton>
-  );
+  return <StyledButton {...props}>{children}</StyledButton>;
 }
 
 export default Button;
